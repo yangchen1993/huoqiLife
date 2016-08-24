220d9ba581077997d2a45bba24e47a2c4fb1624f
yc1993 <yangchen@idianjia.cn>
2016-08-24 16:02:30 +0800

    去掉.idea文件追踪并创建首页

--- js/myController.js
+++ js/myController.js
@@ -7,7 +7,13 @@ myController.controller('mainController',['$scope',function($scope){
     $('#myCarousel').carousel({
         interval: 3000
     });
-    $('.banner i').click(function(){
-        $(this).css('color','#e42220');
-    })
+    $('.circle_logo').click(function(){
+        $('#circle_logo1').css('color','#e3c820');
+        $('#circle_logo2').css('color','#36bcf4');
+        $('#circle_logo3').css('color','#f5672a');
+        $('#circle_logo4').css('color','#e42220');
+        $('.circle_logo').css('border','0');
+        $(this).find('i').css('color','#e42220');
+        $(this).css('border','1px solid #e22220')
+    });
 }]);
