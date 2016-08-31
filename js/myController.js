4a363c757516aef88446a106241f905207f8d782
yc1993 <yangchen@idianjia.cn>
2016-08-31 18:08:52 +0800

    地图测试

--- js/myController.js
+++ js/myController.js
@@ -60,6 +60,55 @@ myController.controller('orderController',['$scope',function($scope){
 }]);
 
 myController.controller('personalController',['$scope',function($scope){
+    var map = new AMap.Map('container',{
+        resizeEnable:true
+    });
 
+    geocoder();
+
+    function geocoder() {
+        var geocoder = new AMap.Geocoder({
+            // city: "028", //城市，默认：“全国”
+            radius: 1000 //范围，默认：500
+        });
+        //地理编码,返回地理编码结果
+        geocoder.getLocation("北京市", function(status, result) {
+            console.log(status);
+            console.log(result);
+            if (status === 'complete' && result.info === 'OK') {
+                geocoder_CallBack(result);
+            }
+        });
+    }
+
+    //地理编码返回结果展示
+    function geocoder_CallBack(data) {
+        var resultStr = "";
+        //地理编码结果数组
+        var geocode = data.geocodes;
+        console.log(geocode[0].location);
+        console.log(geocode[0].location.lat+"---"+geocode[0].location.lng);
+        // for (var i = 0; i < geocode.length; i++) {
+        //     //拼接输出html
+        //     resultStr += "<span style=\"font-size: 12px;padding:0px 0 4px 2px; border-bottom:1px solid #C1FFC1;\">" + "<b>地址</b>：" + geocode[i].formattedAddress + "" + "&nbsp;&nbsp;<b>的地理编码结果是:</b><b>&nbsp;&nbsp;&nbsp;&nbsp;坐标</b>：" + geocode[i].location.getLng() + ", " + geocode[i].location.getLat() + "" + "<b>&nbsp;&nbsp;&nbsp;&nbsp;匹配级别</b>：" + geocode[i].level + "</span>";
+            addMarker(geocode[0]);
+        // }
+        map.setFitView();
+        // document.getElementById("result").innerHTML = resultStr;
+    }
+
+    function addMarker(d) {
+        var marker = new AMap.Marker({
+            map: map,
+            position: [ d.location.getLng(),  d.location.getLat()]
+        });
+        var infoWindow = new AMap.InfoWindow({
+            content: d.formattedAddress,
+            offset: {x: 0, y: -30}
+        });
+        marker.on("mouseover", function(e) {
+            infoWindow.open(map, marker.getPosition());
+        });
+    }
 }]);
 
